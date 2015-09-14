2e5dff279591842eb74719676ff7a312bfd8d960
Jeremy Zahner <zahner@joshmartin.ch>
2015-09-14 17:24:50 +0200

    Fixing some more ruleset violations

--- web/index.php
+++ web/index.php
@@ -13,30 +13,30 @@ $template_vars = array('APP_ENV' => APP_ENV);
 
 $klein->respond(function ($request, $response, $service) use ($klein) {
 // Handle exceptions => flash the message and redirect to the referrer
-	$klein->onError(function ($klein) {
-		$klein->response()->code(400);
-		$klein->response()->send();
-	});
+  $klein->onError(function ($klein) {
+    $klein->response()->code(400);
+    $klein->response()->send();
+  });
 });
 
 if (APP_ENV == 'development') {
 
-	$klein->respond('GET', '/', function ($request) {
+  $klein->respond('GET', '/', function ($request) {
 
-		global $twig, $template_vars;
+    global $twig, $template_vars;
 
-		$template = $twig->loadTemplate('_index.twig');
-		return $template->render($template_vars);
+    $template = $twig->loadTemplate('_index.twig');
+    return $template->render($template_vars);
 
-	});
+  });
 
 } else {
 
-	$klein->respond('GET', '/', function ($request, $response, $service) {
+  $klein->respond('GET', '/', function ($request, $response, $service) {
 
-		$service->render('pages/_index.html');
+    $service->render('pages/_index.html');
 
-	});
+  });
 
 }
 
